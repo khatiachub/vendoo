c3fd3be312cf3e4dec45c084f558064043d2e2a8
khatiachub <101997849+khatiachub@users.noreply.github.com>
2024-12-22 01:09:46 +0400

    Add project files.

---
+++ vendoo/Models/TokenModel.cs
@@ -0,0 +1,9 @@
+﻿namespace vendoo.Models
+{
+    public class TokenModel
+    {
+        public string Email { get; set; }
+        public int Id { get; set; }
+        public string Role { get; set; }
+    }
+}
---
+++ vendoo/Models/UserModel.cs
@@ -0,0 +1,11 @@
+﻿namespace vendoo.Models
+{
+    public class UserModel
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public int Role_id { get; set; }
+    }
+}
